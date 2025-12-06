f2ad0b8485567be2cdeb614831969a931194e3c6
Haneenelasawy <74567160+HaneenElasawy@users.noreply.github.com>
2025-12-07 00:04:20 +0200

    Integrate TextEditor for file operations
    Refactor main function to use TextEditor class for file handling and editing.

--- 6.Texit-Editor-project/main.cpp
+++ 6.Texit-Editor-project/main.cpp
@@ -1,7 +1,27 @@
+#include <iostream>
+#include <string>
+#include "TextEditor.h"
 
-#include "app.h"
+using namespace std;
 
 int main() {
-    app_run();
+    TextEditor editor;
+
+    cout << "Enter file name: ";
+    string name;
+    getline(cin, name);
+
+    bool existed = editor.loadFile(name);
+    if (existed)
+        cout << "File loaded.\n";
+    else
+        cout << "File not found. A new file will be created.\n";
+
+    cout << "Press Enter to start editing...";
+    cin.ignore(10000, '\n');
+
+    editor.edit();
+
+    cout << "Done.\n";
     return 0;
 }
